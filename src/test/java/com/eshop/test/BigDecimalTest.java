a4adcbc738d95a9627829c1bb5a32ed498fd8baa
wangj <icewind4096@gmail.com>
2018-08-12 12:59:17 +0800

    引入Lombok

---
+++ src/test/java/com/eshop/test/BigDecimalTest.java
@@ -0,0 +1,32 @@
+package com.eshop.test;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by windvalley on 2018/7/26.
+ */
+public class BigDecimalTest {
+    @Test
+    public void test1(){
+        System.out.println(0.05+0.01);
+        System.out.println(1.0+0.2);
+        System.out.println(4.015+100);
+        System.out.println(123.3/100);
+    }
+
+    @Test
+    public void test2(){
+        BigDecimal b1 = new BigDecimal(0.05);
+        BigDecimal b2 = new BigDecimal(0.01);
+        System.out.println(b1.add(b2));
+    }
+
+    @Test
+    public void test3(){
+        BigDecimal b1 = new BigDecimal("0.05");
+        BigDecimal b2 = new BigDecimal("0.01");
+        System.out.println(b1.add(b2));
+    }
+}
